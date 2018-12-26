843189460e61a01778f435cfd76ae60903e3b014
shaulib@wincs.cs.bgu.ac.il <bleich.shauli@gmail.com>
2018-12-26 18:48:47 +0200

    2/3 way switch case

--- src/main/java/bgu/spl/net/api/Messages/Logout.java
+++ src/main/java/bgu/spl/net/api/Messages/Logout.java
@@ -2,9 +2,8 @@ package bgu.spl.net.api.Messages;
 
 public class Logout extends Message{
 
-    private short Opcode = 3;
 
-    public Logout(short opcode) {
+    public Logout() {
         super((short)3);
     }
 }

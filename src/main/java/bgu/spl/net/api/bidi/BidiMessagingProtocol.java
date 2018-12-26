f36e32e77b5e2adabd897a3c8fa22d1cf3fe5641
omerkark <omerkark@gmail.com>
2018-12-26 20:07:24 +0200

    hello

--- src/main/java/bgu/spl/net/api/bidi/BidiMessagingProtocol.java
+++ src/main/java/bgu/spl/net/api/bidi/BidiMessagingProtocol.java
@@ -5,7 +5,8 @@ public interface BidiMessagingProtocol<T>  {
 	 * Used to initiate the current client protocol with it's personal connection ID and the connections implementation
 	**/
     void start(int connectionId, Connections<T> connections);
-    
+
+    // look at the protocal they did.
     void process(T message);
 	
 	/**
